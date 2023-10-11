ce2b0e12b3718d2be0261f749d46629fbe628126
Nettking <m@nettking.no>
2023-10-12 00:45:39 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,26 @@
 import subprocess
 import os
 
+def set_script_paths_and_menu_options():
+    common_path = 'sensor_readings' + os.sep + 'soil_moisture' + os.sep + 'process_raw_data'
+    multi_sensor_path = 'sensor_readings' + os.sep + 'multi_sensor'
+    script_paths = [        
+        os.path.join(common_path, 'calculate_error.py'),
+        os.path.join(common_path, 'graph_error.py'),
+        os.path.join(multi_sensor_path, 'graph_data.py'),
+        os.path.join('tools', 'set_headertext.py')
+    ]
+    
+    menu_options = [
+        "1. Calculate Error",
+        "2. Graph Error",
+        "3. Graph data",
+        "4. Set Header Text"
+        
+    ]
+    
+    return script_paths, menu_options
+
 def run_python_script(script_path):
     try:
         subprocess.run(['python', script_path], check=True)
@@ -9,26 +29,11 @@ def run_python_script(script_path):
     except Exception as e:
         print(f"An error occurred while running script {script_path}: {e}")
 
-if __name__ == "__main__":
+def main():
     print("Running AutoTent 1.0.0 Initial Release")
     print("Select a program to run")
-    if os.name == 'posix':  # Linux and macOS
-        script_paths = [
-            'sensor_readings/soil_moisture/process_raw_data/calculate_error.py',
-            'sensor_readings/soil_moisture/process_raw_data/graph_error.py',
-            'tools/set_headertext.py'
-        ]
-    else: # Windows
-        script_paths = [
-            'sensor_readings\soil_moisture\process_raw_data\calculate_error.py',
-            'sensor_readings\soil_moisture\process_raw_data\graph_error.py',
-            'tools\set_headertext.py'
-        ]
-    menu_options = [
-        "1. Calculate Error",
-        "2. Graph Error",
-        "3. Set Header Text"
-    ]
+    
+    script_paths, menu_options = set_script_paths_and_menu_options()
 
     for option in menu_options:
         print(option)
@@ -36,6 +41,9 @@ if __name__ == "__main__":
     selected_number = int(input("Select a program to run: "))
     selected_program = script_paths[selected_number-1] # Subtract 1 due to index start at 0
     run_python_script(selected_program)
+
+if __name__ == "__main__":
+    main()
     
 
 
