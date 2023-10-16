7581a054d6c299f78d61df58ddc26659f62b725a
Nettking <m@nettking.no>
2023-10-16 09:57:00 +0200

    Run on list instead of single image

---
+++ machine_learning/run.py
@@ -0,0 +1,14 @@
+from machine_learning.load import model
+from machine_learning.preprocess import preprocess_image
+import numpy as np
+
+image_paths = ['path/to/your/new/image.jpg']
+for image_path in image_paths:
+    preprocessed_image = preprocess_image(image_path)
+
+    predictions = model.predict(preprocessed_image)
+    class_labels = ['healthy', 'diseased', 'pest-infested']
+    predicted_class = class_labels[np.argmax(predictions)]
+    confidence = predictions[0][np.argmax(predictions)]
+
+    print(f"The plant on {image_path} is {predicted_class} with confidence {confidence:.2f}")
