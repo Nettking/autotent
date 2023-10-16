8f1d0358cdea514fede51c5399483e4691a84a7b
Nettking <m@nettking.no>
2023-10-16 09:57:18 +0200

    Added convolutional layers

--- machine_learning/train.py
+++ machine_learning/train.py
@@ -1,9 +1,33 @@
-import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# Define your model architecture
-model = tf.keras.Sequential([
-    # Define your convolutional layers and other layers here
+num_classes = 2
+dataset_path = 'path/to/training/dataset'
+
+# Define your CNN model architecture
+model = Sequential([
+    # Convolutional Layer 1
+    Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+    MaxPooling2D((2, 2)),
+
+    # Convolutional Layer 2
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+
+    # Convolutional Layer 3 (optional, you can add more if needed)
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+
+    # Flatten the feature maps
+    Flatten(),
+
+    # Fully Connected Layer 1
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+
+    # Output Layer
+    Dense(num_classes, activation='softmax')  # num_classes is the number of classes in your dataset
 ])
 
 # Compile the model with an appropriate loss function and optimizer
@@ -20,7 +44,7 @@ train_datagen = ImageDataGenerator(
 )
 
 train_generator = train_datagen.flow_from_directory(
-    'path/to/training/dataset',
+    dataset_path,
     target_size=(224, 224),  # Adjust to your image size
     batch_size=32,
     class_mode='categorical',
@@ -28,7 +52,7 @@ train_generator = train_datagen.flow_from_directory(
 )
 
 validation_generator = train_datagen.flow_from_directory(
-    'path/to/training/dataset',
+    dataset_path,
     target_size=(224, 224),
     batch_size=32,
     class_mode='categorical',
