4b4ade0890b111bf0bc31d8e6383ea65c69f74b4
Nettking <m@nettking.no>
2023-11-09 11:37:40 +0100

    Update graph_error.py

--- sensor_readings/soil_moisture/process_raw_data/graph_error.py
+++ sensor_readings/soil_moisture/process_raw_data/graph_error.py
@@ -1,14 +1,14 @@
 import csv
 import matplotlib.pyplot as plt
 
-def gather_data_from_csv(csv_filename="datalog.csv"):
+def gather_data_from_csv(csv_filename="sensor_readings/soil_moisture/data/datalog.csv"):
     with open(csv_filename, "r") as f:
         reader = csv.DictReader(f)
         data = list(reader)
     return data
 
-def plot_error_per_day(data, selected_sensors):
-    sensors = set(row["SensorID"] for row in data if row["SensorID"] in selected_sensors)
+def plot_error_per_day(data):
+    sensors = set(row["SensorID"] for row in data)
     days = set(row["Timestamp"] for row in data)
     errors = {sensor: {day: 0 for day in days} for sensor in sensors}
     
@@ -16,45 +16,19 @@ def plot_error_per_day(data, selected_sensors):
         day = row["Timestamp"]
         sensor = row["SensorID"]
         error = abs(float(row["Error"]))
-        if sensor in sensors:
-            errors[sensor][day] = error
+        errors[sensor][day] = error
 
-    num_sensors = len(sensors)
-    fig, axs = plt.subplots(num_sensors, 1, figsize=(10, 4*num_sensors), sharex=True)
-    
-    if num_sensors == 1:
-        axs = [axs]
-    
-    for idx, (sensor, error_data) in enumerate(sorted(errors.items())):
+    for sensor, error_data in errors.items():
         days = sorted(error_data.keys())
         error_values = [error_data[day] for day in days]
-        
-        axs[idx].plot(days, error_values, color='blue')
-        axs[idx].set_ylabel("Error Value")
-        axs[idx].set_title(f"Sensor {sensor}")
-        
-    axs[-1].set_xlabel("Day")
-    fig.suptitle("Error per Day for Selected Sensors", fontsize=16, y=1.02)
-    plt.tight_layout()
+        plt.plot(days, error_values, label=sensor)
+
+    plt.legend(loc="upper left")
+    plt.xlabel("Day")
+    plt.ylabel("Error Value")
+    plt.title("Error per Day for each Sensor")
     plt.show()
 
 if __name__ == "__main__":
     data = gather_data_from_csv()
-    
-    print("Select a range of sensors to display:")
-    print("1. A0-A2")
-    print("2. A3-A6")
-    print("3. A7-A9")
-    choice = input("Enter your choice (1, 2, or 3): ")
-    
-    if choice == "1":
-        selected_sensors = [f"A{i}" for i in range(3)]
-    elif choice == "2":
-        selected_sensors = [f"A{i}" for i in range(3, 7)]
-    elif choice == "3":
-        selected_sensors = [f"A{i}" for i in range(7, 10)]
-    else:
-        print("Invalid choice. Exiting.")
-        exit()
-    
-    plot_error_per_day(data, selected_sensors)
+    plot_error_per_day(data)
